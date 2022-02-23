cb8f3ffe642ab6a9d15cd1aae7c9d4a4977b8aae
Ebrahimbeydagi <80033303+Ebrahimbeydagi@users.noreply.github.com>
2022-02-23 10:31:50 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import TopGroup from "./topGroup";
-import {makeStyles} from "@mui/styles";
 import FifthTable from "./FifthTable";
 
-const useStyle=makeStyles({
-    containerDiv:{
-
-    }
-});
-
 const App = () => {
-    const classes = useStyle();
     return (
-        <div className={classes.containerDiv}>
+        <div>
             <TopGroup/>
             <FifthTable/>
         </div>
